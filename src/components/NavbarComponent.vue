fdd33beda79beb1eb1b19f400ca8230c74d07fa0
ican-graphics-01 <lankaican@gmail.com>
2023-01-12 12:23:30 +0530

    hero section vue init

---
+++ src/components/NavbarComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="bg-gray-900 text-gray-100 py-8 px-6 shadow md:flex justify-between items-center">
+    <div class="flex items-center cursor-pointer">
+        <span class="text--green-500 text-xl mr-1">Portfolio.CNT</span>
+    </div>
+
+    <ul >
+        <li>
+            <a href="#">Home</a>
+        </li>
+    </ul>
+
+  </div>
+</template>
+
+<script>
+
+
+
+export default {
+    name: "NavbarView",
+}
+</script>
+
+<style>
+
+</style>
