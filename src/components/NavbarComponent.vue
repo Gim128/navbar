7fc59e8549b6a666f16121e532f3bae6a82496a5
Gim128 <gimhanpabasara4@gmail.com>
2023-01-12 15:31:33 +0530

    completely done & dusted navbar using vue

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -4,22 +4,53 @@
         <span class="text--green-500 text-xl mr-1">Portfolio.CNT</span>
     </div>
 
-    <ul >
-        <li>
-            <a href="#">Home</a>
-        </li>
-    </ul>
+    <span @click="MenuOpen()" class="absolute md:hidden right-6 top-1.5 cursor-pointer text-4xl">
+      <v-icon :class="[open ? 'mdi-menu' : 'mdi-format-align-left']"></v-icon>
+    </span>
 
+    <ul class="md:flex md:items-end md:px-0 px-10 md:pb-0 pb-10 md:static absolute bg-gray-900 md:w-auto w-full top-14 duration-700 ease-in" :class="[open ? 'left-0' : 'left-[-100%]']">
+        <li class="md:mx-4 md:my-0 my-6" v-for="link in Links" :key="link">
+            <a :href="link.link" class="text-xl hover:text-gold-500">{{link.name}}</a>
+        </li>
+
+        <button-component />
+
+    </ul>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue'
+import buttonComponent from './buttonComponent.vue'
 
 
 
 export default {
+  components: { buttonComponent },
     name: "NavbarView",
-}
+    
+    
+
+    setup() {
+      let open = ref(true)
+      let Links = [
+        {name: "Home", link: "#"},
+        {name: "Home", link: "#"},
+        {name: "Home", link: "#"},
+        {name: "Home", link: "#"},
+      ]
+
+      function MenuOpen() {
+        open.value = !open.value
+      }
+
+      return {Links, open, MenuOpen}
+    },
+
+
+  }
+
+
 </script>
 
 <style>
---
+++ src/components/buttonComponent.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="text-center">
+        <v-menu data-app offset-y>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              data-app
+              color="#0652DD"
+              dark
+              v-bind="attrs"
+              v-on="on"
+            >
+              Language
+            </v-btn>
+          </template>
+          <v-list data-app >
+            <v-list-item data-app
+              v-for="(item, index) in items"
+              :key="index"
+            >
+              <v-list-item-title data-app >{{ item.title }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </div>
+</template>
+
+<script>
+export default {
+    name: "ButtonView",
+
+    data: () => ({
+      items: [
+        { title: 'English' },
+        { title: 'Chinese' },
+      ],
+    }),
+}
+</script>
+
+<style>
+
+</style>
